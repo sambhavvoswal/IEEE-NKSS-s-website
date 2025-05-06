d5268bad99f4841d3ad28b37fbb56e27861cb055
sambhav oswal <sambhavoswal23@gmail.com>
2025-05-06 22:15:03 +0530

    removed readme(extra copy)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./index.css" 
+import Card from "./components/card"
+
+function App() {
+
+  return (
+    <>
+      <Card />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <h1>We are processing the things</h1>
+  )
+}
+
+export default Card
